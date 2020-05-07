9ce8c130b1ab02779b452a7d9f3f23d675763d65
koftov <ahronlu@gmail.com>
2020-05-07 18:21:38 +0300

    Updates

---
+++ precache-manifest.4221adad3c8e5a008ac52fa2db82d5bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97f92c4f4451d462563115e6f5f1eb4",
+    "url": "/chucknorrisjokes/index.html"
+  },
+  {
+    "revision": "c8dd08728d487fb280c1",
+    "url": "/chucknorrisjokes/static/js/2.65ba919b.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/chucknorrisjokes/static/js/2.65ba919b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d0c2c4db543d59b8dc",
+    "url": "/chucknorrisjokes/static/js/main.97d7d332.chunk.js"
+  },
+  {
+    "revision": "91c71f73fa4dcbb9450a",
+    "url": "/chucknorrisjokes/static/js/runtime-main.e2330114.js"
+  }
+]);
